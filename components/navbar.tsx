1a7b5bfb966a196fee96f7581cd27e493e4267a9
tanay-vaswani <vaswani.tanay9@gmail.com>
2023-11-29 03:37:10 +0530

    navbar some more..

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,22 +1,25 @@
 import { LogIn, User } from "lucide-react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between w-full py-4 px-4 shadow-sm shadow-blue-600 backdrop-blur-sm">
-      <div className="text-lg font-bold">Navbar</div>
+      <Link href="/" className="text-2xl font-bold hover:text-blue-600">
+        medoff
+      </Link>
       <div className="flex gap-x-2">
         <Button
           variant={"ghost"}
-          className="shadow-sm shadow-blue-500 hover:text-blue-500"
+          className="shadow-sm shadow-blue-500 hover:text-blue-600"
         >
-          <LogIn className="h-4 w-4 mr-2" /> Login
+          <LogIn className="h-4 w-4 mr-2 hidden md:block" /> Login
         </Button>
         <Button
           variant={"ghost"}
-          className="shadow-sm shadow-blue-500 hover:text-blue-500"
+          className="shadow-sm shadow-blue-500 hover:text-blue-600"
         >
-          <User className="h-4 w-4 mr-2" /> SignUp
+          <User className="h-4 w-4 mr-2 hidden md:block" /> SignUp
         </Button>
       </div>
     </div>
